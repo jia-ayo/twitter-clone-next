c3eafd6af109f1f542e07cad05e83b91e142a80d
jia-ayo <milkjia11@gmail.com>
2023-06-23 17:06:55 +0100

    sidebar init

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import FollowBar from "./Layout/FollowBar";
 import Sidebar from "./Layout/Sidebar";
 
 interface LayoutProps {
@@ -12,6 +13,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
           <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
             {children}
           </div>
+          <FollowBar/>
         </div>
       </div>
     </div>
